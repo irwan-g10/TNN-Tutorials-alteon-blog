64d8bc7169374d4b2f2105d33eaa06d8703d4aab
Irwan Gumilar <irwangumilar111@gmail.com>
2022-04-27 13:54:29 +0700

    adding isPending

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,27 @@ const Home = () => {
 
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(response => {
                 return response.json()
             })
             .then((data) => {
                 // return console.log(data)
                 setBlogs(data)
-            })
+                setIsPending(false)
+            }) 
+        }, 1000);
     }, [])
 
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );
